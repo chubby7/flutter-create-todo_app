8f25abe23271ae1a8abff36bc71cbb86034d5e87
Dimma <dominicsavio4allmen@gmail.com>
2026-06-11 16:58:49 +0100

    complete Todo app UI and add TextEditingController

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'constants.dart';
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -9,77 +10,135 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  TextEditingController taskController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    taskController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                height: 200,
-                width: 400,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0B132B),
-                  border: Border.all(width: 0.3, color: Colors.white),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Todo Done',
-                            style: TextStyle(
-                              fontSize: 40.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.25,
+              decoration: BoxDecoration(
+                color: Color(0xFF0B132B),
+                border: Border.all(width: 0.3, color: Colors.white),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Todo Done',
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Container(
-                            height: 120,
-                            width: 120,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(70),
-                              color: Colors.white,
-                            ),
-                            child: Center(
-                              child: Text(
-                                '5/8',
-                                style: TextStyle(
-                                  color: Color(0xFF0B132B),
-                                  fontSize: 40.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                        ),
+                        Container(
+                          height: 120,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(70),
+                            color: Colors.white,
+                          ),
+                          child: Center(
+                            child: Text(
+                              '5/8',
+                              style: TextStyle(
+                                color: Color(0xFF0B132B),
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                      Text(
-                        'June 10, 2026',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                    Text('June 10, 2026', style: kNormalTextStyle),
+                  ],
                 ),
               ),
-              Row(
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
+              child: Row(
                 children: [
-                  Expanded(child: TextField()),
-                  Icon(Icons.add),
+                  Expanded(
+                    child: TextField(
+                      controller: taskController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        hintText: 'Input a new task',
+                        filled: true,
+                        fillColor: Colors.grey.shade900,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  IconButton(
+                    icon: Icon(Icons.add_circle, size: 50),
+                    onPressed: () {
+                      print(taskController.text);
+                      taskController.clear();
+                    },
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5,
+              ),
+              child: Text('Tasks', style: kNormalTextStyle),
+            ),
+            TasksList(title: 'buy fertilizer'),
+            TasksList(title: 'plant okra'),
+            TasksList(title: 'sell beans'),
+            TasksList(title: 'rent tractor'),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+  TasksList({required this.title});
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade900,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListTile(
+        leading: Checkbox(value: false, onChanged: (value) {}),
+        title: Text(title, style: TextStyle(color: Colors.white)),
+        trailing: Icon(Icons.delete, color: Colors.red),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
        home: InputPage(),
     );
   }
